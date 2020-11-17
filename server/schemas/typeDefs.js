6e05c24b200e6812c782ec84f0ebbb326d2cbdaf
jack <biffisbuffm@gmail.com>
2020-11-17 16:16:50 -0600

    getposts hits

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,14 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    posts: [Post]
+    
+}
+
 type Post {
     _id: ID
     name: String!
@@ -10,11 +18,20 @@ type Post {
     location: String!
     createdAt: String
     username: String
+    comments: [Comment]
     commentCount: Int
-    
 
     
 }
+
+type Comment {
+    _id: ID
+    commentBody: String!
+    username: String
+    createdAt: String
+}
+
+
 
 type Query {
     posts: [Post]
