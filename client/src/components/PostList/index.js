619a232f3f24dc1c9651e3c3ff7da5a2cfdc0594
jack <biffisbuffm@gmail.com>
2020-11-19 15:34:28 -0600

    adding posts list

---
+++ client/src/components/PostList/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button, Card } from 'semantic-ui-react';
+import moment from 'moment';
+
+
+const PostCard = ({ posts: { name, type, location, createdAt, id, comments}}) => {
+// if (!posts.length) {
+//     return <h3>No Added Providers</h3>
+// }
+
+    //if there are posts, generate card 
+    return (
+        <Card fluid>
+              <Card.Content>
+                <Card.Header>{name}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
+                <Card.Description>
+                    <ul>
+                        <li>{type}</li>
+                        <li>{location}</li>
+                        
+                    
+                    </ul>
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+            <p>Reviews:{comments}</p>
+
+            <Button>
+                Leave Review
+            </Button>
+            
+            </Card.Content>
+        </Card>
+    )
+
+
+
+}
+    
+
+
+export default PostCard;
