1b3f992a7e8a8e8060f6f7eeb56885983d658051
jack <biffisbuffm@gmail.com>
2020-11-24 16:01:31 -0600

    sec

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -22,7 +22,7 @@ module.exports = {
     }
 
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, process.env.secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token from line 28');
